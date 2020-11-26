47141180a5b2b279a5df2fdf64dfa58d4005b130
Alotaibi <74378111+itserrozz@users.noreply.github.com>
2020-11-25 23:48:00 -0800

    Add files via upload

---
+++ Tellonym Grabber/Tellonym Grabber/Form1.Designer.vb
@@ -0,0 +1,223 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Black
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(5, 201)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(63, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Black
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(67, 201)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(65, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(2, 21)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "Username"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(2, 45)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "Password"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BackColor = System.Drawing.Color.Black
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.ForeColor = System.Drawing.Color.White
+        Me.TextBox3.Location = New System.Drawing.Point(2, 93)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(134, 102)
+        Me.TextBox3.TabIndex = 4
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Black
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(0, 250)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Users : 0"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Black
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(0, 267)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Status : Not logged"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.Font = New System.Drawing.Font("Leelawadee", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label3.Location = New System.Drawing.Point(-1, 2)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(277, 14)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Tellonym Grabber                                                        "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Black
+        Me.Label4.Font = New System.Drawing.Font("Leelawadee", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Fuchsia
+        Me.Label4.Location = New System.Drawing.Point(121, 2)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(15, 14)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "X"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BackColor = System.Drawing.Color.Black
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox4.ForeColor = System.Drawing.Color.White
+        Me.TextBox4.Location = New System.Drawing.Point(2, 69)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(134, 20)
+        Me.TextBox4.TabIndex = 9
+        Me.TextBox4.Text = "Target"
+        Me.TextBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.BackColor = System.Drawing.Color.Black
+        Me.RadioButton2.ForeColor = System.Drawing.Color.White
+        Me.RadioButton2.Location = New System.Drawing.Point(67, 227)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(69, 17)
+        Me.RadioButton2.TabIndex = 11
+        Me.RadioButton2.Text = "Following"
+        Me.RadioButton2.UseVisualStyleBackColor = False
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.BackColor = System.Drawing.Color.Black
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.ForeColor = System.Drawing.Color.White
+        Me.RadioButton1.Location = New System.Drawing.Point(3, 228)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(65, 17)
+        Me.RadioButton1.TabIndex = 10
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Random"
+        Me.RadioButton1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(137, 284)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "By @404.erroz"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+End Class
---
+++ Tellonym Grabber/Tellonym Grabber/Form1.vb
@@ -0,0 +1,212 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Net
+Imports System.Threading
+Imports Microsoft.VisualBasic.CompilerServices, System.Text.RegularExpressions
+''' instagram @404.erroz
+'''github : https://github.com/itserrozz
+Public Class Form1
+    Dim sttop As Boolean
+    Dim bb As Boolean
+    Dim pp As Point
+    Dim token As String
+    Dim attempts As Integer
+    Public Sub New()
+        Me.attempts = 0
+        InitializeComponent()
+    End Sub
+    Private Sub Label3_MouseDown(sender As Object, e As MouseEventArgs) Handles Label3.MouseDown
+        If e.Button = MouseButtons.Left Then
+            Me.pp = New Point(0 - e.X, 0 - e.Y)
+            Me.bb = True
+        End If
+    End Sub
+
+    Private Sub Label3_MouseMove(sender As Object, e As MouseEventArgs) Handles Label3.MouseMove
+        If Me.bb Then
+            Dim mousePosition As Point = Control.MousePosition
+            mousePosition.Offset(Me.pp.X, Me.pp.Y)
+            MyBase.Location = mousePosition
+        End If
+    End Sub
+
+    Private Sub Label3_MouseUp(sender As Object, e As MouseEventArgs) Handles Label3.MouseUp
+        If e.Button = MouseButtons.Left Then
+            Me.bb = False
+        End If
+    End Sub
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If Not TextBox2.Text = "Password" Then
+            TextBox2.UseSystemPasswordChar = True
+        Else
+            TextBox2.UseSystemPasswordChar = False
+        End If
+    End Sub
+    Public Function login(username As String, password As String) As Object
+        Try
+            Dim utf As New UTF8Encoding()
+            Dim bytes As Byte() = utf.GetBytes("{""deviceName"":""Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"",""deviceType"":""web"",""lang"":""en"",""captcha"":""03AGdBq25_lFdGVNvBoCdEKv2tbXuDctiPorxexi4wuKyYbgz2hSF1rdhE6QBu2-KQ53_-b-nyJIKwXLCz8LL_xp2hQ5WaRU0jcmjxCAbCs-96X1ZfC0JcEd7ZrINjoQwMl6yRGAiLRW6FZUxl3NmnfU8aHwj2fTEGg8D1ZBjYQPnrfGVi1FQX7vvAkLpSGk3y3sriFSUrrjruHEDrdL4pDxJmmYJkVmIJVnhSDjgKGVvLgpTBlcR_vexnv6zvHQX89YGnWcOcp-c-gnqB3eTDaQoV2YVVMu7N2bObkpWQcBhJIG-5UxD5dAgEoI_DHbiNSTkNBrdzfGzPQWwLRHh8HSLNfMtiOEKKBJvufcq8T0G7Yj1FlSmCBGDsGZnekrNf-oF3NioeLrWW"",""email"":""" & username & """,""password"":""" & password & """,""limit"":25}")
+            Dim httpwebrequest As HttpWebRequest = CType(WebRequest.Create("https://api.tellonym.me/tokens/create"), HttpWebRequest)
+            httpwebrequest.Method = "POST"
+            httpwebrequest.Proxy = Nothing
+            httpwebrequest.Host = "api.tellonym.me"
+            httpwebrequest.UserAgent = "Mozilla/5.0 (Linux; Android 7.1.1; G8231 Build/41.2.A.0.219; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/59.0.3071.125 Mobile Safari/537.36"
+            httpwebrequest.KeepAlive = True
+            httpwebrequest.ContentType = "application/json;charset=utf-8"
+            httpwebrequest.ContentLength = CLng(bytes.Length)
+            Dim requestStream As Stream = httpwebrequest.GetRequestStream()
+            requestStream.Write(bytes, 0, bytes.Length)
+            requestStream.Close()
+            Dim text As String = New StreamReader(httpwebrequest.GetResponse().GetResponseStream()).ReadToEnd()
+            Me.token = Regex.Match(text, """accessToken"":""(.*?)"",""").Groups(1).Value
+            If text.Contains("accessToken") Then
+                Label2.Text = "Status : login Successfully"
+            End If
+        Catch ex As WebException
+            ProjectData.SetProjectError(ex)
+            Dim ex2 As WebException = ex
+            Dim textt2 As String = (String.Concat(New StreamReader(ex2.Response.GetResponseStream).ReadToEnd))
+            If textt2.Contains("Wrong credentials") Then
+                Label2.Text = "Status : Password incorrect!"
+            End If
+
+        End Try
+    End Function
+    Public Sub get_userID()
+        If Not token = Nothing Then
+            Label2.Text = "Status : Starting .."
+        Else
+            MsgBox("Please Login First!", MsgBoxStyle.Critical)
+        End If
+        Try
+            Dim httpwebrequest As HttpWebRequest = CType(WebRequest.Create("https://api.tellonym.me/profiles/name/" + TextBox4.Text), HttpWebRequest)
+            httpwebrequest.Method = "GET"
+            httpwebrequest.Proxy = Nothing
+            httpwebrequest.Host = "api.tellonym.me"
+            httpwebrequest.UserAgent = "Tellonym/377 CFNetwork/758.5.3 Darwin/15.6.0"
+            httpwebrequest.KeepAlive = True
+            httpwebrequest.ContentType = "application/json;charset=utf-8"
+            Dim httpresponse As HttpWebResponse
+            httpresponse = DirectCast(httpwebrequest.GetResponse(), HttpWebResponse)
+            Dim reader As New StreamReader(httpresponse.GetResponseStream())
+            Dim text As String = reader.ReadToEnd()
+            Dim userID As String = Regex.Match(text, "id"":(.*?),").Groups(1).Value
+            Dim a As Thread = New Thread(AddressOf get_followers)
+            a.Start(userID)
+        Catch ex As WebException
+            ProjectData.SetProjectError(ex)
+            Dim ex2 As WebException = ex
+            Dim text_error As String = (String.Concat(New StreamReader(ex2.Response.GetResponseStream).ReadToEnd))
+            If text_error.Contains("NOT_FOUND") Then
+                MsgBox("Not Found Username!")
+            ElseIf text_error.Contains("This account is banned.") Then
+                MsgBox("Username Banned !")
+            End If
+
+        End Try
+    End Sub
+    Public Sub get_followers(userID As String)
+
+        Try
+            Dim httpwebrequest As HttpWebRequest = CType(WebRequest.Create("https://api.tellonym.me/followers/id/" + userID + "?userId=" + userID + "&adExpId=82&limit=16"), HttpWebRequest)
+            httpwebrequest.Method = "GET"
+            httpwebrequest.Proxy = Nothing
+            httpwebrequest.Host = "api.tellonym.me"
+            httpwebrequest.UserAgent = "Tellonym/377 CFNetwork/758.5.3 Darwin/15.6.0"
+            httpwebrequest.KeepAlive = True
+            httpwebrequest.Headers.Add("authorization", "Bearer " & token)
+            httpwebrequest.ContentType = "application/json;charset=utf-8"
+            Dim httpresponse As HttpWebResponse
+            httpresponse = DirectCast(httpwebrequest.GetResponse(), HttpWebResponse)
+            Dim reader As New StreamReader(httpresponse.GetResponseStream())
+            Dim text As String = reader.ReadToEnd()
+            If text.Contains("{""followers"":[]}") Then
+                MsgBox("Don't have Followers!")
+            Else
+
+                Dim user_regex As MatchCollection = Regex.Matches(text, "username"":""(.*?)"",")
+                For Each obj As Object In user_regex
+                    Dim id As String = Regex.Match(text, "id"":(.*?),").Groups(1).Value
+                    Dim value_json As Match = CType(obj, Match)
+                    Dim user As String = value_json.Groups(1).Value.ToString()
+                    TextBox3.AppendText(user & vbCrLf)
+                    attempts += 1
+                    Label1.Text = "Users : " + attempts.ToString()
+                    My.Computer.FileSystem.WriteAllText("Usernames.txt", user + vbCrLf, True)
+                Next
+                MsgBox("Finshed ! " + attempts.ToString)
+            End If
+        Catch ex As WebException
+            ProjectData.SetProjectError(ex)
+            Dim ex2 As WebException = ex
+            Dim text_error As String = (String.Concat(New StreamReader(ex2.Response.GetResponseStream).ReadToEnd))
+            MsgBox(text_error, MsgBoxStyle.Critical)
+        End Try
+    End Sub
+    Public Sub random_grab()
+        While True
+            If Not token = Nothing Then
+                Label2.Text = "Status : Starting .."
+            Else
+                MsgBox("Please Login First!", MsgBoxStyle.Critical)
+                Exit While
+            End If
+            Thread.Sleep(1500)
+            Try
+                Dim httpwebrequest As HttpWebRequest = CType(WebRequest.Create("https://api.tellonym.me/suggestions/people"), HttpWebRequest)
+                httpwebrequest.Method = "GET"
+                httpwebrequest.Proxy = Nothing
+                httpwebrequest.Host = "api.tellonym.me"
+                httpwebrequest.UserAgent = "Tellonym/377 CFNetwork/758.5.3 Darwin/15.6.0"
+                httpwebrequest.KeepAlive = True
+                httpwebrequest.Headers.Add("authorization", "Bearer " & token)
+                httpwebrequest.ContentType = "application/json;charset=utf-8"
+                Dim httpresponse As HttpWebResponse
+                httpresponse = DirectCast(httpwebrequest.GetResponse(), HttpWebResponse)
+                Dim reader As New StreamReader(httpresponse.GetResponseStream())
+                Dim text As String = reader.ReadToEnd()
+                Dim user_regex As MatchCollection = Regex.Matches(text, "username"":""(.*?)"",")
+                For Each obj As Object In user_regex
+                    Dim id As String = Regex.Match(text, "id"":(.*?),").Groups(1).Value
+                    Dim tcg As Match = CType(obj, Match)
+                    Dim user As String = tcg.Groups(1).Value.ToString()
+                    TextBox3.AppendText(user & vbCrLf)
+                    My.Computer.FileSystem.WriteAllText("Usernames.txt", user + vbCrLf, True)
+                    attempts += 1
+                    Label1.Text = "Users : " + attempts.ToString()
+                Next
+            Catch ex As WebException
+                ProjectData.SetProjectError(ex)
+                Dim ex2 As WebException = ex
+                Dim text_error As String = (String.Concat(New StreamReader(ex2.Response.GetResponseStream).ReadToEnd))
+                If text_error.Contains("RATELIMIT_REACHED") Then
+                    MsgBox("Finshed ! " + attempts.ToString)
+                    Exit While
+                End If
+            End Try
+        End While
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim user As String = TextBox1.Text
+        Dim password As String = TextBox2.Text
+        login(user, password)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+        MsgBox("welcome .." + vbCrLf + "instagram : @404.erroz" + vbCrLf + "github : itserrozz", MsgBoxStyle.Information, "Codded By @404.erroz")
+    End Sub
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+        ProjectData.EndApp()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If RadioButton1.Checked = True Then
+            Dim a As Thread = New Thread(AddressOf random_grab)
+            a.Start()
+        Else
+            Dim a2 As Thread = New Thread(AddressOf get_userID)
+            a2.Start()
+        End If
+    End Sub
+End Class
